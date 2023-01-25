c571c537a00071e98f55a54385ffaea3e1ae87d4
Pacifisity <97253406+Pacifisity@users.noreply.github.com>
2023-01-25 12:02:22 -0500

    Update money_calc.py

--- files/money_calc.py
+++ files/money_calc.py
@@ -1,4 +1,7 @@
 class Money:
+    def __init__(self):
+        self.payroll = Payroll()
+
     def round_and_format_amount(self, amount: float) -> str:
         # Rounded the amount to 2 decimal places and format it as a string.
         # If the rounded amount is greater than or equal to 1, it returns "${:.2f}" format.
@@ -14,13 +17,17 @@ class Money:
 
     def part_of_whole_percentage(self, whole: float, part: float) -> str:
         # Calculate the percentage of the part to the whole
-        return f"{(part / whole)*100:.2f}%"
+        return f"{round((part / whole)*100)}%"
     
-    def calculate_percentage_of_number(self, percent: float, number: float) -> str:
+    def percent_of_number_percentage(self, percent: float, number: float) -> str:
         # Calculate the value of a percent of a number
         return self.round_and_format_amount((percent / 100) * number)
+    
+    def calculate_salary_plus_comission(self, salary: float, comission_percentage: float, sold_products: float):
+        return f"${salary + round(sold_products * (comission_percentage/100), 2)}"
 
     def calculate_is_of(self, string):
+        # Var init
         is_index = string.find("is")
         of_index = string.find("of")
         if is_index == -1 or of_index == -1:
@@ -28,6 +35,7 @@ class Money:
         is_num = ""
         of_num = ""
 
+        # Find the numbers near is and of
         for i in range(is_index + 3, len(string)): # start from index after "is "
             if string[i].isnumeric() or string[i] == ".":
                 is_num += string[i]
@@ -39,25 +47,59 @@ class Money:
                 of_num += string[i]
             else:
                 break
-
+        
+        # No instance of is or of
         if is_num == "" or of_num == "":
             return None
 
         is_num = float(is_num)
         of_num = float(of_num)
 
+        # Divide by zero prevention
         if of_num == 0:
             return None
 
+        # Check for percentage
         if '%' in string[is_index:]:
             return f"{(is_num * of_num)/100}"
         else:
             return f"{(is_num / of_num)*100:.2f}%"
 
+class Payroll:
+    def __init__(self):
+        self.biweekly = 26
+        self.semimonthly = 24
+    
+    # Calculates pay given the hour and rate then formats it as a string
+    def calculate_pay(self, hours: float, rate: float):
+        if hours > 40:
+            total_pay = self.calculate_overtime(hours, rate)
+        else:
+            total_pay = hours * rate
+
+        if total_pay == round(total_pay):
+            return f"${round(total_pay)}"
+        else:
+            return f"${round(total_pay, 2)}"
+    
+    # Calculate's overtime pay given the hour and rate then returns it as a number
+    def calculate_overtime(self, hours: float, rate: float):
+        overtime_hours = hours - 40
+        overtime_pay = overtime_hours * (rate * 1.5)
+        regular_pay = 40 * rate
+        total_pay = regular_pay + overtime_pay
+        return total_pay
+
+    # Calculates the hourly rate given the amount in hours and the gross pay
+    def calculate_hourly_rate(self, hours: float, gross: float):
+        return f"${round(gross / hours, 2)}/hr"
+
 money = Money()
-# print(money.calculate_price_per_pound(1.73, 0.3))
-# print(money.part_of_whole_percentage(120, 42))
-# print(money.calculate_percentage_of_number(65, 1150))
-question = input("Question:\n")
-calculation = money.calculate_is_of(question)
-print(calculation)
+
+question1 = money.payroll.calculate_pay(3*3, 17.15)
+question2 = money.payroll.calculate_hourly_rate(8+7+5+1+8, 617.09)
+question3 = money.payroll.calculate_pay(43, 23.44)
+question4 = money.part_of_whole_percentage(134, 14)
+question5 = money.calculate_salary_plus_comission(1815, 4, 9283)
+
+print(f"1: {question1}\n2: {question2}\n3: {question3}\n4: {question4}\n5: {question5}")
